c0e96fdd17d2f63b3182a08c34b2feab4e9688d9
shreejaydivate13 <shreejaydivate13@gmail.com>
2026-03-20 13:47:45 +0530

    Add files via upload

---
+++ Activity4b.py
@@ -0,0 +1,15 @@
+# -*- A shopping app stores product prices in a list. Find total bill. -*-
+"""
+Created on Fri Mar 20 13:35:35 2026
+
+@author: Shreejay
+"""
+
+# List of product prices
+prices = [100, 250, 75, 30]
+
+# Calculate total bill
+total_bill = sum(prices)
+
+# Display result
+print("Total Bill:", total_bill)
