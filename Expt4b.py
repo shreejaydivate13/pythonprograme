abab5eda54ea5fb3148679036bee9759992a5d99
shreejaydivate13 <shreejaydivate13@gmail.com>
2026-03-20 13:00:52 +0530

    Add files via upload

---
+++ Expt4b.py
@@ -0,0 +1,12 @@
+# -*- Lists and Strings -*-
+"""
+Created on Fri Mar 20 12:44:02 2026
+
+@author:Shreejay
+"""
+# Take input from the user
+string = input("Enter a string: ")
+# Reverse the string using slicing
+reversed_string = string[::-1]
+# Print the result
+print("Reversed string:", reversed_string)
