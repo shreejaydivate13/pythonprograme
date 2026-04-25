0da3389ee62fc11e0f20f129e4ed5339a1694018
shreejaydivate13 <shreejaydivate13@gmail.com>
2026-04-25 12:09:18 +0530

    Add files via upload

---
+++ activity1(expt5).py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 11:52:34 2026
+
+@author: Shreejay Divate
+"""
+# Creating a dictionary
+library = {}
+
+# Adding book names and prices
+library["Math"] = 150
+library["Science"] = 200
+library["English"] = 180
+
+print("Before updating:", library)
+
+# Updating price of Science book
+library["Science"] = 250
+
+print("After updating:", library)
