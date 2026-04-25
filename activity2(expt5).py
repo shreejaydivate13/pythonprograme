4b057b0d2395bd54146199596d92e17c13afefa0
shreejaydivate13 <shreejaydivate13@gmail.com>
2026-04-25 12:17:12 +0530

    Add files via upload

---
+++ activity2(expt5).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 12:10:05 2026
+
+@author: SIDDHANT
+"""
+# Roll numbers of students in first class
+class1 = {1, 2, 3, 4, 5}
+
+# Roll numbers of students in second class
+class2 = {3, 4, 5, 6, 7}
+
+# Students present in both classes
+both_classes = class1 & class2
+
+print("Students present in both classes:", both_classes)
